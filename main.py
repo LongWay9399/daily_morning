313a0def1a513ab77cad199b5e60743e857d885b
longway <eb@ebdeMacBook-Pro-3.local>
2022-09-16 13:34:16 +0800

    改日期

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import random
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
+wedding_date = os.environ['WEDDING_DATE']
+marriage_date = os.environ['MARRIAGE_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
 
@@ -19,35 +21,62 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+    res = requests.get(url).json()
+    weather = res['data']['list'][0]
+    return weather['weather'], math.floor(weather['temp'])
+
 
 def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
+    delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+    return delta.days
+
 
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+    next = datetime.strptime(str(date.today().year) +
+                             "-" + birthday, "%Y-%m-%d")
+    if next < datetime.now():
+        next = next.replace(year=next.year + 1)
+    return (next - today).days
+
+
+def get_marriage():
+    next = datetime.strptime(str(date.today().year) +
+                             "-" + marriage_date, "%Y-%m-%d")
+    if next > datetime.now():
+        return "距离领证还有"+(today-next).days+"天"
+    return "距离下个结婚纪念日还有"+(next.replace(year=next.year + 1)-today).days+"天"
+
+
+def get_wedding():
+    delta = datetime.strptime(wedding_date, "%Y-%m-%d")
+    if delta > datetime.now():
+        return ""
+    return "距离举行婚礼还有"+(today-delta).days+"天"
+
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+    words = requests.get("https://api.shadiao.pro/chp")
+    if words.status_code != 200:
+        return get_words()
+    return words.json()['data']['text']
+
 
 def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
+    return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+
+data = {"weather": {"value": wea, "color": get_random_color()},
+        "temperature": {"value": temperature, "color": get_random_color()},
+        "love_days": {"value": get_count(), "color": get_random_color()},
+        "birthday_left": {"value": get_birthday(), "color": get_random_color()},
+        "wedding_left": {"value": get_wedding(), "color": get_random_color()},
+        "wedding_left": {"value": get_marriage(), "color": get_random_color()},
+        "words": {"value": get_words(), "color": get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
